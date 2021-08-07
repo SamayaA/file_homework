eb7e59dd30310200c6c27dc963e961e68f0f5032
SamayaA <asadullaevasamaya@gmail.com>
2021-08-07 17:29:07 +0300

    Modified get_shop_list_by_dishes(). Fixed if.

--- o_file.py
+++ o_file.py
@@ -1,6 +1,7 @@
-
 from pprint import pprint
+import os
 
+os.chdir('/Users/Samaya/Desktop/Netology/OOP/file_system/')
 with open('data.txt' , 'r', encoding="UTF8") as file :
     cook_book = dict()
     for line in file:
@@ -16,15 +17,16 @@ with open('data.txt' , 'r', encoding="UTF8") as file :
         file.readline()
  
 def get_shop_list_by_dishes(dishes, person_count):
-	ingredients = dict()
-	for i in range(0 , len(dishes)):
-		dish_ingredients = cook_book.get(dishes[i])
-		for j in range(0 , len (dish_ingredients)):
-			ingredient_name = dish_ingredients[j]['ingredient_name']
-			if (ingredients.get(ingredient_name) == None):
-				ingredients[ingredient_name] = dict()
-				ingredients[ingredient_name]['measure'] = dish_ingredients[j]['measure']
-				ingredients[ingredient_name]['quantity'] = dish_ingredients[j]['quantity']
-			ingredients[ingredient_name]['quantity'] += dish_ingredients[j]['quantity']
-	return ingredients
-				
+  ingredients = dict()
+  for i in range(0 , len(dishes)):
+    dish_ingredients = cook_book.get(dishes[i])
+    for j in range(0 , len (dish_ingredients)):
+      ingredient_name = dish_ingredients[j]['ingredient_name']
+      if (ingredients.get(ingredient_name) == None):
+        ingredients[ingredient_name] = dict()
+        ingredients[ingredient_name]['measure'] = dish_ingredients[j]['measure']
+        ingredients[ingredient_name]['quantity'] = dish_ingredients[j]['quantity'] * person_count
+      else :
+        ingredients[ingredient_name]['quantity'] += dish_ingredients[j]['quantity'] * person_count
+  return ingredients
+    
